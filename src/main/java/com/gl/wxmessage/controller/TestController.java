bd48d3ab4d655655a5280809de4afe82966607dd
gaolei <gaolei@ysten.com>
2022-12-04 16:22:32 +0800

    Init Project

---
+++ src/main/java/com/gl/wxmessage/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.gl.wxmessage.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gaolei
+ * @date 2022/12/04
+ */
+@RestController
+@RequestMapping
+@Slf4j
+public class TestController {
+
+    @GetMapping
+    public void test() {
+        log.info("test!");
+    }
+}
